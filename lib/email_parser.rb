46b6720e0273a41296d1b4cda935116da66b7d61
Renee Nordholm <reneenordholm@gmail.com>
2019-05-19 19:15:05 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -17,3 +17,11 @@ class EmailParser
     @@parse.each { |email| puts email.parse.split(",") }
   end
 end
+
+
+# def self.new_from_filename(name)
+#     song = self.new 
+#     song.name = (name.split(" - ")[1].chomp(".mp3"))
+#     song.artist_name = (name.split(" - ")[0])
+#     song
+#   end
